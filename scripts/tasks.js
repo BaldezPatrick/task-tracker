1ac41c3814832c8edb8bd940570528ac01545970
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-05 15:21:55 -0300

    feat: Function to create a new task

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -2,3 +2,11 @@ const inputTask = document.querySelector("#task");
 const sendTask = document.querySelector("#send-task");
 const listTask = document.querySelector(".tasks-items-wrapper");
 
+const createTask = (textValue) => {
+  const li = createListItem();
+  li.textContent = textValue;
+  listTask.appendChild(li);
+  cleanUpInput();
+  createDeleteButton(li);
+  saveTasks();
+};
