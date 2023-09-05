16b79e7af42f5912a70b4f872f74a138cecb7fad
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-05 15:22:39 -0300

    feat: Function to clean up the input task after create task

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -2,6 +2,12 @@ const inputTask = document.querySelector("#task");
 const sendTask = document.querySelector("#send-task");
 const listTask = document.querySelector(".tasks-items-wrapper");
 
+
+const cleanUpInput = () => {
+  inputTask.value = "";
+  inputTask.focus();
+};
+
 const createTask = (textValue) => {
   const li = createListItem();
   li.textContent = textValue;
