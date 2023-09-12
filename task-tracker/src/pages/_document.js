db857c6c5bdcd1f1f62d418b9d9893f176b21c35
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-12 18:07:12 -0300

    feat: Add some meta tags and title to the search engines

--- task-tracker/src/pages/_document.js
+++ task-tracker/src/pages/_document.js
@@ -3,11 +3,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Task Tracker is a simple web application designed to help you remember and manage your daily tasks. You can easily add and delete tasks as you perform them"
+        />
+        <title>Task Tracker</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
