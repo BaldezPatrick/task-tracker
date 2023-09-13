58493446e088a29c9c24f50c81557dff7e85be46
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-13 02:59:09 -0300

    refactor: move the task form to a reusable component (taskForm);

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import styles from "@/styles/Home.module.css";
 import { useState } from "react";
 import Notifications from "@/components/notifications";
 import TaskList from "@/components/taskList";
+import TaskForm from "@/components/taskForm";
 
 export default function Home() {
   const [tasks, setTasks] = useState([]);
@@ -56,17 +57,11 @@ export default function Home() {
       <main className="main-wrapper">
         <section className="tasksForm-wrapper">
           <h3>Remember your taks and do them!</h3>
-          <form onSubmit={addTask}>
-            <input
-              type="text"
-              placeholder="New task..."
-              value={newTask}
-              onChange={(e) => setNewTask(e.target.value)}
-            />
-            <button id="send-task" type="submit">
-              Send
-            </button>
-          </form>
+          <TaskForm
+            addTask={addTask}
+            newTask={newTask}
+            setNewTask={setNewTask}
+          />
         </section>
         <section className="tasks-wrapper">
           <h3>Your tasks</h3>
