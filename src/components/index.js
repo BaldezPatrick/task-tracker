5a9be49bb4c842e91d028dfe978a8bb5069aad25
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-13 17:10:32 -0300

    feat: Search input for the task when you type the name or letter

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Notifications from "./notifications";
 import TaskForm from "./taskForm";
 import TaskList from "./taskList";
+import TaskSearch from "./taskSearch";
 
-export { Notifications, TaskForm, TaskList };
+export { Notifications, TaskForm, TaskSearch, TaskList };
