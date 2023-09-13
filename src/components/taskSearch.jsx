d5304e7b450d49fd5f0a8307b0776d3c2eebd8f0
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-13 17:25:45 -0300

    fix: for a better semantic html

--- src/components/taskSearch.jsx
+++ src/components/taskSearch.jsx
@@ -1,13 +1,15 @@
 const TaskSearch = ({ inputFilterTask, setInputFilterTask }) => {
   return (
     <>
-      <input
-        type="text"
-        placeholder="Search"
-        value={inputFilterTask}
-        onChange={(e) => setInputFilterTask(e.target.value)}
-      />
-      <button onClick={(e) => setInputFilterTask("")}>Clean search</button>
+      <form>
+        <input
+          type="text"
+          placeholder="Search"
+          value={inputFilterTask}
+          onChange={(e) => setInputFilterTask(e.target.value)}
+        />
+        <button onClick={(e) => setInputFilterTask("")}>Clean search</button>
+      </form>
     </>
   );
 };
