901e65d75d7368e3d67d2777cdaca598e8c2b1fa
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-13 17:08:35 -0300

    feat: taskList now has a filter task

--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -1,16 +1,25 @@
-const TaskList = ({ tasks, deleteTask, editTask }) => {
+const TaskList = ({ tasks, deleteTask, editTask, filterTask }) => {
+  const filteredTasks = tasks.filter((task) =>
+    task.title.toLowerCase().includes(filterTask.toLowerCase())
+  );
   return (
     <>
       <ul className="tasks-items-wrapper">
-        {tasks.map((task) => (
-          <li key={task.id} className="tasks-item">
-            {task.title}
-            <div className="buttons-wrapper">
-              <button onClick={() => editTask(task.id)}>Edit</button>
-              <button onClick={() => deleteTask(task.id)}>Delete</button>
-            </div>
+        {filteredTasks.length === 0 ? (
+          <li>
+            <p>No task was found...</p>
           </li>
-        ))}
+        ) : (
+          filteredTasks.map((task) => (
+            <li key={task.id} className="tasks-item">
+              {task.title}
+              <div className="buttons-wrapper">
+                <button onClick={() => editTask(task.id)}>Edit</button>
+                <button onClick={() => deleteTask(task.id)}>Delete</button>
+              </div>
+            </li>
+          ))
+        )}
       </ul>
     </>
   );
