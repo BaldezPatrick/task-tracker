2994cb88f9d7ec90cf7cc0a767d3cfa4c8ee90e9
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-12 23:08:31 -0300

    fix: Changing the location where the meta tag is inserte 'cause in the _document file it is only rendered the in the initial pre-render

--- task-tracker/src/pages/_document.js
+++ task-tracker/src/pages/_document.js
@@ -3,16 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="description"
-          content="Task Tracker is a simple web application designed to help you remember and manage your daily tasks. You can easily add and delete tasks as you perform them"
-        />
-        <title>Task Tracker</title>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
